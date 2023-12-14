e9cd2496eb6ec31a2be0e06ee56aaae1f82f15c5
slauniainen <samuli.launiainen@luke.fi>
2023-12-14 16:07:35 +0200

    added demo Notebook

--- test_horizon.py
+++ test_horizon.py
@@ -9,51 +9,63 @@ import numpy as np
 import matplotlib.pyplot as plt
 import dem_horizon as h
 
-# import dem example
-demfile = r'sve_1_dem_16m_aggr.asc'
+def run_demo():
 
-dem, lat, lon, _ = h.read_AsciiGrid(demfile)
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import dem_horizon as h
+    # import dem example
+    demfile = r'sve_1_dem_16m_aggr.asc'
 
-#plt.figure(1)
-#plt.imshow(dem)
-#plt.xlabel('Lon ix') # column index
-#plt.ylabel('Lat ix') # row index
-#cb = plt.colorbar()
-#cb.set_label('Elev [m]')
+    dem, lat, lon, _ = h.read_AsciiGrid(demfile)
+
+    #plt.figure(1)
+    #plt.imshow(dem)
+    #plt.xlabel('Lon ix') # column index
+    #plt.ylabel('Lat ix') # row index
+    #cb = plt.colorbar()
+    #cb.set_label('Elev [m]')
 
 
-# set parameters
-dx = 16.0 # dem resolution (m)
-view_dist = 500.0 # viewing distance
-sectors = 36.0 # sectors = 360 (deg) / azimuth interval (deg)
+    # set parameters
+    dx = 16.0 # dem resolution (m)
+    view_dist = 500.0 # viewing distance
+    sectors = 36 # sectors = 360 (deg) / azimuth interval (deg)
 
-# create Horizon object
-H = h.Horizon(dem, dx=16)
-print('rows, cols', H.shape)
+    # create Horizon object
+    H = h.Horizon(dem, dx=16)
+    print('rows, cols', H.shape)
 
-# let's compute horizon for point Lat_ix = 40, Lon_ix 50
-lat_ix = 140
-lon_ix = 60
-P0 = h.Point(lat_ix, lon_ix, dem[lat_ix, lon_ix])
+    # let's compute horizon for point Lat_ix = 40, Lon_ix 50
+    lat_ix = 140
+    lon_ix = 60
+    P0 = h.Point(lat_ix, lon_ix, dem[lat_ix, lon_ix])
 
-# calculate horizon height angle (deg)
-Az = np.linspace(0, 360, sectors)
+    # calculate horizon height angle (deg)
+    Az = np.linspace(0, 360, sectors)
 
-Az, elev_angle, terrain_profile = H.calc_horizon(P0, R=view_dist, Az_deg=Az, figs=True)
+    Az, elev_angle, terrain_profile = H.calc_horizon(P0, R=view_dist, Az_deg=Az, figs=True)
 
-# plot elevation profiles for few directions
+    # plot elevation profiles for few directions
 
-Az_ix = np.arange(0, len(Az), 5)
+    Az_ix = np.arange(0, len(Az), 5)
 
-plt.figure()
+    fig = plt.figure()
 
-for k in range(len(Az_ix)):
-    y = (terrain_profile['elev'][Az_ix[k]])
-    x = np.arange(0, len(y)) * dx
-    s = '%.1f deg' % terrain_profile['Az'][Az_ix[k]]
-    plt.plot(x, y, '-', label=s)
+    for k in range(len(Az_ix)):
+        y = (terrain_profile['elev'][Az_ix[k]])
+        x = np.arange(0, len(y)) * dx
+        s = '%.1f deg' % terrain_profile['Az'][Az_ix[k]]
+        plt.plot(x, y, '-', label=s)
 
-plt.legend()
-plt.title('terrain profiles')
-plt.ylabel('elev [m]')
-plt.xlabel('distance from target [m]')
+    plt.legend()
+    plt.title('terrain profiles')
+    plt.ylabel('elev [m]')
+    plt.xlabel('distance from target [m]')
+   
+    fig.savefig('horizon.png')
+    fig.show()
+    return terrain_profile
+
+if __name__ == "__main__":
+    run_demo()
